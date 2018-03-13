2bc73f9f2d2ec95780f294340dd2f88a9cd88e84
Sandy Edwards <peterjamesedwards@gmail.com>
2018-03-13 16:41:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -44,6 +44,6 @@ var chocolateBars = [
   }
   
   function removeElementFromEndOfArray(array) {
-    array.slice(0,array.length - 1);
+    array = array.slice(0,array.length - 1);
     return array
   }
