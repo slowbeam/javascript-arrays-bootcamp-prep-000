3c7ff1d7894c5a7909f579935a84e09ac2440394
Sandy Edwards <peterjamesedwards@gmail.com>
2018-03-13 16:36:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,3 +37,8 @@ var chocolateBars = [
     array = array.slice(1);
     return array;
   }
+  
+  function destructivelyRemoveElementFromEndOfArray(array) {
+    array.pop();
+    return array;
+  } 
